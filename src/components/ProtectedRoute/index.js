f6319177fefcc0c960103120ffae8735f5b37c26
Srilakshmi <srilakshmid46@gmail.com>
2023-03-03 11:24:00 +0000

    Jobby App Updated Solution

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,16 +1,3 @@
-// import {Redirect, Route} from 'react-router-dom'
-// import Cookie from 'js-cookie'
-
-// const ProtectedRoute = props => {
-//   const token = Cookie.get('jwt_token')
-//   if (token === undefined) {
-//     return <Redirect to="/login" />
-//   }
-//   return <Route {...props} />
-// }
-
-// export default ProtectedRoute
-
 import {Redirect, Route} from 'react-router-dom'
 import Cookies from 'js-cookie'
 
